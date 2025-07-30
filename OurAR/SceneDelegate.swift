efb78a895dcc367aa2e78f7cd6601e2c8b35c16e
zonglikang <604922726@qq.com>
2025-07-30 11:16:18 +0800

    代码优化：常量-变量

--- OurAR/SceneDelegate.swift
+++ OurAR/SceneDelegate.swift
@@ -19,7 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        guard let _ = (scene as? UIWindowScene) else { return }
         
         if scene.isKind(of: UIWindow.self) {
-            var window = UIWindow()
+            let window = UIWindow()
             self.window = window
             window.makeKeyAndVisible()
         }

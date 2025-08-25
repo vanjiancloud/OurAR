06ae0b7864fe63bc4965d8b119b932cd0c7bfbad
zonglikang <604922726@qq.com>
2025-08-25 10:31:39 +0800

    漫游增加重力和碰撞检测

--- OurAR/Main/Tool/ManYou/ManYou.swift
+++ OurAR/Main/Tool/ManYou/ManYou.swift
@@ -12,6 +12,8 @@ class ManYou: GTView {
     var manYouName: UILabel!
     var slider: UISlider!
     var manYouValue: UILabel!
+    var leftButton: UIButton!
+    var rightButton: UIButton!
     
     init(frame: CGRect) {
         super.init(frame: frame, titleName: "漫游导航")
@@ -55,10 +57,73 @@ class ManYou: GTView {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
             self.slider.setValue(4, animated: false)
         }
+        
+        leftButton = UIButton(frame:CGRectZero)
+        leftButton.contentMode = .scaleAspectFit
+        let targetImageSize = CGSize(width: 16, height: 16)
+        if let normalImage = UIImage(named: "remenberuncheck"),
+           let selectedImage = UIImage(named: "remenbercheck") {
+                let resizedNormalImage = resizeImage(image: normalImage, targetSize: targetImageSize)
+                let resizedSelectedImage = resizeImage(image: selectedImage, targetSize: targetImageSize)
+            
+            leftButton.setImage(resizedSelectedImage, for: .selected)
+            leftButton.setImage(resizedNormalImage, for: .normal)
+        }
+        leftButton.isSelected = true
+        leftButton.setTitle(" 重力", for: .selected)
+        leftButton.setTitle(" 重力", for: .normal)
+        leftButton.setTitleColor(.white, for: .normal)
+        leftButton.setTitleColor(.white, for: .selected)
+        leftButton.titleLabel?.font = .systemFont(ofSize: 14)
+        leftButton.addAction(UIAction(handler: {_ in
+            self.leftClick()
+        }),for: .touchUpInside)
+        addSubview(leftButton)
+        leftButton.snp.makeConstraints { make in
+            make.top.equalTo(slider.snp.bottom).offset(12)
+            make.left.equalTo(slider.snp.left)
+            make.size.equalTo(CGSizeMake(55, 20))
+        }
+        
+        rightButton = UIButton(frame:CGRectZero)
+        rightButton.contentMode = .scaleAspectFit
+        if let normalImage = UIImage(named: "remenberuncheck"),
+           let selectedImage = UIImage(named: "remenbercheck") {
+                let resizedNormalImage = resizeImage(image: normalImage, targetSize: targetImageSize)
+                let resizedSelectedImage = resizeImage(image: selectedImage, targetSize: targetImageSize)
+            
+                rightButton.setImage(resizedSelectedImage, for: .selected)
+                rightButton.setImage(resizedNormalImage, for: .normal)
+        }
+        rightButton.isSelected = true
+        rightButton.setTitle(" 碰撞", for: .selected)
+        rightButton.setTitle(" 碰撞", for: .normal)
+        rightButton.setTitleColor(.white, for: .normal)
+        rightButton.setTitleColor(.white, for: .selected)
+        rightButton.titleLabel?.font = .systemFont(ofSize: 14)
+        rightButton.addAction(UIAction(handler: {_ in
+            self.rightClick()
+        }),for: .touchUpInside)
+        addSubview(rightButton)
+        rightButton.snp.makeConstraints { make in
+            make.centerY.equalTo(leftButton)
+            make.left.equalTo(leftButton.snp.right).offset(12)
+            make.size.equalTo(CGSizeMake(55, 20))
+        }
     
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleBackgroundTap))
         tapGesture.cancelsTouchesInView = true
         addGestureRecognizer(tapGesture)
+    }
+    
+    private func leftClick() {
+        leftButton.isSelected = !leftButton.isSelected
+        self.sendManYouAction()
+    }
+    
+    private func rightClick() {
+        rightButton.isSelected = !rightButton.isSelected
+        self.sendManYouAction()
     }
     
     @objc private func handleBackgroundTap() {
@@ -72,6 +137,22 @@ class ManYou: GTView {
     //MARK: 展示分解页面时调用
     func open() {
 
+    }
+    
+    func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage? {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let scale = min(widthRatio, heightRatio)
+        
+        let scaledSize = CGSize(width: size.width * scale, height: size.height * scale)
+        
+        let renderer = UIGraphicsImageRenderer(size: scaledSize)
+        return renderer.image { _ in
+            image.draw(in: CGRect(origin: .zero, size: scaledSize))
+        }
     }
     
     @objc private func sliderValueChanged(_ slider: UISlider?,for event: UIEvent?) {
@@ -94,16 +175,35 @@ class ManYou: GTView {
                 break
             case .ended:
                 print("slider end")
-                sendManYou(enableGravity: "false", enableAllCollision: "false", value: Int(slider.value), completion: {result in
-                    if !self.isHidden && !result {
-                        if let parentView = self.superview {
-                            showTip(tip: "指令下发失败", parentView: parentView,tipColor_bg_fail,tipColor_text_fail) {}
-                        }
-                    }
-                })
+                self.sendManYouAction()
             default:
                 break
             }
         }
+    }
+    
+    private func sendManYouAction() {
+        var enableGravity: String
+        var enableAllCollision: String
+        
+        if leftButton.isSelected {
+            enableGravity = "true"
+        } else {
+            enableGravity = "false"
+        }
+        
+        if rightButton.isSelected {
+            enableAllCollision = "true"
+        } else {
+            enableAllCollision = "false"
+        }
+        
+        sendManYou(enableGravity: enableGravity, enableAllCollision: enableAllCollision, value: Int(slider.value), completion: {result in
+            if !self.isHidden && !result {
+                if let parentView = self.superview {
+                    showTip(tip: "指令下发失败", parentView: parentView,tipColor_bg_fail,tipColor_text_fail) {}
+                }
+            }
+        })
     }
 }
